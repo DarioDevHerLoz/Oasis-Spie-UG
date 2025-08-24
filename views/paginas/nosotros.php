dd2668354daba61490549494184cf75353f0f0aa
Coronilla <e.coronillahernandez@ugto.com>
2025-08-23 20:52:16 -0600

    Comienzo de front Nosotros

--- views/paginas/nosotros.php
+++ views/paginas/nosotros.php
@@ -1,3 +1,49 @@
 <main class="">
-  <h1>Aqui va lo del Chichon(Coronilla)</h1>
+  <section class="nosotros">
+  <div class="container">
+    <h2 class="nosotros__title">
+      Sobre Nosotros<span class="nosotros__dot">.</span>
+    </h2>
+
+    <div class="nosotros__intro">
+        <img src="../../src/img/equipo.png" class="nosotros__photo">
+
+      <div class="nosotros__desc">
+        <p>Somos un grupo estudiantil apasionado por la óptica, la fonética, la astronomía y la ciencia en general. Nuestra misión es fomentar el interés y la difusión del conocimiento en estas áreas a través de actividades académicas, divulgativas y experimentales.</p>
+        <p>Formamos parte de una comunidad universitaria comprometida con el aprendizaje y la innovación. Organizamos conferencias, talleres, observaciones astronómicas, experimentos de óptica y proyectos de investigación que permiten a nuestros miembros desarrollar habilidades científicas y técnicas, además de fortalecer el trabajo en equipo.</p>
+        <p>Buscamos inspirar a futuros científicos e ingenieros, promoviendo el pensamiento crítico y la curiosidad científica . Nuestro objetivo es conectar a estudiantes con expertos del campo, ofreciendo oportunidades para el crecimiento académico y profesional dentro de un entorno colaborativo.</p>
+        <p>Si te apasiona la luz, el universo y la ciencia en todas sus formas, ¡Te invitamos a unirte a nuestea comunidad y explorar con nosotros los secretos de la naturaleza!</p>
+      </div>
+    </div>
+
+    <div class="nosotros__grid">
+      <div class="nosotros__card">
+        <span class="nosotros__label">Misión <span class="nosotros__dot">.</span></span>
+        <div class="nosotros__circle">
+          <img src="../../src/img/misión.png" alt="">
+        </div>
+      </div>
+      <div class="nosotros__card">
+        <span class="nosotros__label">Visión <span class="nosotros__dot">.</span></span>
+        <div class="nosotros__circle">
+          <img src="../../src/img/visión.png" alt="">
+        </div>
+      </div>
+      <div class="nosotros__card">
+        <span class="nosotros__label">Valores <span class="nosotros__dot">.</span></span>
+        <div class="nosotros__circle">
+          <img src="../../src/img/misión.png" alt="">
+        </div>
+      </div>
+      <div class="nosotros__card">
+        <span class="nosotros__label">Objetivos <span class="nosotros__dot">.</span></span>
+        <div class="nosotros__circle">
+          <img src="../../src/img/misión.png" alt="">
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section>
+
 </main>
